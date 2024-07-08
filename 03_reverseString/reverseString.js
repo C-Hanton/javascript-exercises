eb70ae5a964313861eca845c1e6b5d52dac3d5d7
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2024-07-08 10:11:10 +0100

    add function to reverseString.js to fulfil test requirements of reverseString.speec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+let splitString = input.split("");
+splitString.reverse();
+let revInput = splitString.join("");
+return revInput;
 };
 
 // Do not edit below this line
