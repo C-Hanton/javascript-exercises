ff1045e0ca33ed7fe989e1075556e72054f6aa94
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2024-07-08 15:25:38 +0100

    add pseudocode to explain my solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -9,5 +9,19 @@ const removeFromArray = function(arr , ...args) {
     return newArray;
 };
 
+//Steps of the above code
+/*
+1. ...args is used to represent infinite number of arguments to potentially be given
+2.create a new array on line 2
+3. line 4 forEach cycles through the initial array
+4. if statement checks if args does not match item
+5. if they dont match, item is pushed to new array
+6. new array is returned
+
+
+
+
+
+*/
 // Do not edit below this line
 module.exports = removeFromArray;
