a32d590bbf5a9fa99d17ad4d6a6f00f5b167d8db
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2024-07-09 18:02:39 +0100

    amended leapYears.js to fulfil test requirements in leapYears.spec.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+let isDivisibleByFour = year % 4 === 0;
+let isCentury = year % 100 === 0;
+let isDivisibleByFourHundred = year % 400 === 0;
+
+if (isDivisibleByFour && (!isCentury || isDivisibleByFourHundred)){
+    return true;
+} else {
+    return false;
+}
+
 
 };
 
