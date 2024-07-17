4446169d551982f3bac2685642bc7647ffacb748
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2024-07-17 12:29:18 +0100

    add code to calculator.js to fulfil test requirements of calculator.spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,24 +3,33 @@ const add = function(a, b) {
   return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    let result = a - b;
+  return result;
+  
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total,current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+return array.reduce((total, current) => total * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = Math.pow(a, b);
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0){
+    return -1;
+  } else if (num === 0){
+    return 1;
+  } else {
+    return num * (factorial (num - 1))
+  }
 };
 
 // Do not edit below this line
