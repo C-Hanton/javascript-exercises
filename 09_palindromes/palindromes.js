50defb919201538f8d97d979996d299696ae3e1e
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2024-07-17 13:52:29 +0100

    add functions to fulfil test requriements

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphaNumberical = "1234567890abcdefghijklmnopqrstuvwxyz"
 
+    let cleanString = str
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphaNumberical.includes(character))
+        .join('')
+
+
+    let reversedString = cleanString.split('').reverse().join('')
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
